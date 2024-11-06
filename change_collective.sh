1065dee8e95dbe6014d9d117c925834acae41c0e
DaXor-0 <saverio.pasqualoni@yahoo.it>
2024-11-06 18:19:15 +0100

    modified change_collective.sh

--- change_collective.sh
+++ change_collective.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-location='local'
+# location='local'
 # location='leonardo'
-# location='snellius'
+location='snellius'
 
 if [ $location == 'leonardo' ]; then
-    RULE_UPDATER_EXEC=/leonardo/home/userexternal/spasqual/Swing_Test/update_collective_rules
-    RULE_FILE_PATH=/leonardo/home/userexternal/spasqual/Swing_Test/collective_rules.txt
+    RULE_UPDATER_EXEC=$HOME/Swing_Test/update_collective_rules
+    RULE_FILE_PATH=$HOME/Swing_Test/collective_rules.txt
 elif [ $location == 'snellius' ]; then
-    RULE_UPDATER_EXEC=./update_collective_rules
-    RULE_FILE_PATH=/home/spasqualoni/Swing_Test/collective_rules.txt
+    RULE_UPDATER_EXEC=$HOME/Swing_Test/update_collective_rules
+    RULE_FILE_PATH=$HOME/Swing_Test/collective_rules.txt
 elif [ $location == 'local' ]; then
     RULE_UPDATER_EXEC=./update_collective_rules
-    RULE_FILE_PATH=/home/saverio/University/Tesi/test/collective_rules.txt
+    RULE_FILE_PATH=$HOME/University/Tesi/test/collective_rules.txt
 else
     echo "ERROR: location not correctly set up, aborting..."
     exit 1
