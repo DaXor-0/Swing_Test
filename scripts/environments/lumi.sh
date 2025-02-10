09a576d6f06b8f2408763474e935b22e43b65322
DaXor-0 <saverio.pasqualoni@yahoo.it>
2025-02-10 16:25:16 +0100

    added suboptimal tag to swing-allreduce-bdw memcpy, datatype and segmented, added suboptimal to exclude in example and test

---
+++ scripts/environments/lumi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Variables always needed
+export RUN=srun
+export RUNFLAGS=
+export SWING_DIR=$HOME/Swing_Test
+
+
+# Used to load python and virtual environment
+load_python() {
+# It contains
+# - python-3.11.5
+# - numpy-1.24.4
+# - scipy-1.10.1
+# - mpi4py-3.1.4
+# - dask-2023.6.1
+    module load cray-python/3.11.5 || { error "Failed to load Python module." ; return 1; }
+    return 0
+}
+
+# Load environment variables dependant on the MPI library
+load_other_env_var() {
+    if [ "$MPI_LIB" == "OMPI_SWING" ]; then
+        export PATH=$HOME/bin:$PATH
+        export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
+        export MANPATH=$HOME/share/man:$MANPATH
+    fi
+
+    if [[ "$MPI_LIB" == "OMPI_SWING" ]] || [[ "$MPI_LIB" == "OMPI" ]]; then
+        export OMPI_MCA_coll_hcoll_enable=0
+        export OMPI_MCA_coll_tuned_use_dynamic_rules=1
+        if [ "$CUDA" == "False" ]; then
+            export CUDA_VISIBLE_DEVICES=""
+            export OMPI_MCA_btl="^smcuda"
+            export OMPI_MCA_mpi_cuda_support=0
+        fi
+    fi
+}
