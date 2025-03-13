a0158cb185b4c17027118aac1b21498e0af4dd8a
DaXor-0 <127130888+DaXor-0@users.noreply.github.com>
2025-03-13 12:06:40 +0100

    lorenzo e' un maiale

--- bench/bench_utils.h
+++ bench/bench_utils.h
@@ -49,7 +49,7 @@
 #define CUDA_CHECK(cmd) do {                         \
   cudaError_t e = cmd;                              \
   if( e != cudaSuccess ) {                          \
-    printf("Failed: Cuda error %s:%d '%s'\n",             \
+    fprintf(stderr, "Failed: Cuda error %s:%d '%s'\n",             \
         __FILE__,__LINE__,cudaGetErrorString(e));   \
     exit(EXIT_FAILURE);                             \
   }                                                 \
